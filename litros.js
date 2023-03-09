e5ae3f0915d96e3a74aafc7dbd4077fe89d3bb69
danielvictor11 <127199051+danielvictor11@users.noreply.github.com>
2023-03-09 09:43:06 -0300

    Delete litros.js

--- litros.js
+++
@@ -1,9 +0,0 @@
-function calcular () {
-    var tempo = parseFloat(document.getElementById("tempo").value);
-    var velocidade = parseFloat(document.getElementById("velocidade").value);
-    var distancia = tempo*velocidade*velocidade
-    var litro = distancia/12
-    alert("o litro ultilizado é  " + litro);
-    console.log("litro " + litro);
-    
-}
