85836c228315572307fd5ae7333165cad4eb84ea
danielvictor11 <127199051+danielvictor11@users.noreply.github.com>
2023-03-09 07:46:53 -0300

    Add files via upload

---
+++ CalculoMatos.js
@@ -0,0 +1,21 @@
+function media(){
+    var nota1 = parseFloat(document.getElementById("nota1").value);	
+    var nota2 = parseFloat (document.getElementById("nota2").value);
+    
+    var media = (nota1 + nota2)/2 ;
+    
+    if (media >= 7 ){ 
+       if (media==10){
+       alert("Uau! Aprovado");
+      console.log ('aprovado'+media);
+      }
+      else{
+           alert("parabens, aprovado! meda "+media);
+      }
+    }
+     else{
+           alert("reprovado!")
+    } 
+
+    
+    }
