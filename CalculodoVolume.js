69aaa5533d18996261880bd206ef471295e25a66
danielvictor11 <127199051+danielvictor11@users.noreply.github.com>
2023-03-09 08:58:15 -0300

    Delete CalculodoVolume.js

--- CalculodoVolume.js
+++
@@ -1,8 +0,0 @@
-function calculoA () {
-    var altura = parseFloat(document.getElementById("altura").value);
-    var Raio = parseFloat(document.getElementById("raio").value);
-    var volume = altura*3.14159*Raio*2 
-    alert("o calculo do volume é  " + volume);
-    console.log("volume " + volume);
-    
-}
